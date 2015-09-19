525537852d4a08b751841b0c9d544a99d6401d33
pejuan <jose.pejuan@unitec.edu>
2015-09-19 10:38:08 -0600

    Agregada top bar y maneja mejor los logins

--- HospitalPJ/app/controllers/doctors_controller.rb
+++ HospitalPJ/app/controllers/doctors_controller.rb
@@ -1,6 +1,6 @@
 class DoctorsController < ApplicationController
   before_action :set_doctor, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_admin!
   # GET /doctors
   # GET /doctors.json
   def index
