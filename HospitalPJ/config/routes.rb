04220d3a2c5b95b46902f5983d82ff2d02e51bcf
pejuan <jose.pejuan@unitec.edu>
2015-09-20 09:41:30 -0600

    Terminado los logins

--- HospitalPJ/config/routes.rb
+++ HospitalPJ/config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  root 'doctors#index'
+  root 'microbiologists#new'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
